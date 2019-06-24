a7449b0ba688decc73f0110e13a8a3f1fe10bc1a
kurochan <kuro@kurochan.org>
2019-06-25 01:50:34 +0900

    skip broken bytes and decode

--- src/main/scala/org/kurochan/scaptive_portal/openflow/handler/MessageDecoder.scala
+++ src/main/scala/org/kurochan/scaptive_portal/openflow/handler/MessageDecoder.scala
@@ -4,7 +4,8 @@ import com.typesafe.scalalogging.StrictLogging
 import io.netty.buffer.ByteBuf
 import io.netty.channel.ChannelHandlerContext
 import io.netty.handler.codec.ByteToMessageDecoder
-import org.projectfloodlight.openflow.protocol.OFFactories
+import org.projectfloodlight.openflow.exceptions.OFParseError
+import org.projectfloodlight.openflow.protocol.{OFFactories, OFMessage}
 
 import scala.util.{Failure, Success, Try}
 
@@ -28,8 +29,7 @@ class MessageDecoder() extends ByteToMessageDecoder with StrictLogging {
         }
         case Failure(e) => {
           logger.error(s"MessageDecoder ERROR!: ${e.getMessage}")
-          in.readByte()
-          None
+          skipBrokenBytesAndDecode(in)
         }
       }
     }.takeWhile(_.nonEmpty)
@@ -38,5 +38,36 @@ class MessageDecoder() extends ByteToMessageDecoder with StrictLogging {
       case Some(msg) => out.add(msg)
       case None =>
     }
+  }
+
+  private def skipBrokenBytesAndDecode(in: ByteBuf): Option[OFMessage] = {
+    val indexBeforeSkip = in.readerIndex()
+    var continue = true
+    var lastMsg: Option[OFMessage] = None
+
+    while (continue && in.isReadable) {
+      val ofMessageTry = Try(Option(reader.readFrom(in)))
+
+      ofMessageTry match {
+        case Success(ofMessage) => {
+          continue = false
+          lastMsg = ofMessage
+        }
+        case Failure(_: OFParseError) => {
+          in.readByte()
+        }
+        case Failure(_: IllegalArgumentException) => {
+          in.readByte()
+        }
+        case Failure(e) => {
+          logger.error(s"unknown error", e)
+          in.readByte()
+        }
+      }
+    }
+
+    logger.warn(s"MessageDecoder: ${in.readerIndex() - indexBeforeSkip} bytes discarded. isReadable: ${in.isReadable} (${in.readableBytes()} bytes), last message: ${lastMsg}")
+
+    lastMsg
   }
 }
